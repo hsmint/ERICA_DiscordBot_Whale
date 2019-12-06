2261294450d1fbf72283bf5aad3a3b259ff41641
hsmint <hongsm990724@gmail.com>
2019-12-06 17:05:32 +0900

    Made function new deck

--- game.py
+++ game.py
@@ -4,3 +4,12 @@ class blackjack:
     def __init__(self):
         self.score = 0
         self.deck = []
+    
+    def new_deck(self):
+        suits = {"Spade", "Heart", "Diamond", "Club"}
+        ranks = {'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'}
+        for name in suits:
+            for num in ranks:
+                self.deck.append({"suit": name, "rank": num})
+        random.shuffle(self.deck)
+        return self.deck
