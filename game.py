e8cd23dc2f77d34f40505a263c10dadc1068aab5
hsmint <hongsm990724@gmail.com>
2019-12-09 11:19:55 +0900

    Made count_score function

--- game.py
+++ game.py
@@ -2,7 +2,6 @@ import random
 
 class blackjack:
     def __init__(self):
-        self.score = 0
         self.deck = []
     
     def new_deck(self):
@@ -14,7 +13,23 @@ class blackjack:
         random.shuffle(self.deck)
         return self.deck
     
-    def hit(self, deck):
+    def hit(self):
         if self.deck == []:
             self.deck = new_deck()
         return (self.deck[0] , self.deck[1:])
+    
+    def count_score(self, cards):
+        score = 0
+        number_of_ace = 0
+        for card in cards:
+            save = card['rank']
+            if card['rank'] in ('J', 'Q', 'K'):
+                save = 10
+            elif card['rank'] == 'A':
+                save = 11
+                number_of_ace += 1
+            score += save
+        while number_of_ace > 0 and score > 21:
+            score -= 10
+            number_of_ace -= 1
+        return score
