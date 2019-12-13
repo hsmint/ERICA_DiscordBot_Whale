630cbf61820f0987d8770f493d6987a868af59da
hsmint <hongsm990724@gmail.com>
2019-12-13 18:17:38 +0900

    Adding NOTICE & yt function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
-import os, random
+import os, random, asyncio
+import youtube_dl
 from discord.ext import commands
 
 token = os.environ["discord_auth"]
@@ -7,6 +8,48 @@ token = os.environ["discord_auth"]
 bot = commands.Bot(command_prefix="!",help_command=None, activity=discord.Activity(name="Whale | !help", type=1))
 
 footer = "Made by hsmint"
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 def message(head, msg):
     e = discord.Embed(title=head, description=msg)
@@ -45,6 +88,10 @@ class Game(commands.Cog):
         e = message("<Showing Game available>", text)
         await ctx.send(embed=e)
     
+    @commands.command()
+    async def blackjack(self, ctx):
+        return
+
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
